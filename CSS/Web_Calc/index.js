feaa53bbac8b92c7965e22b92e94c670fc758e4e
Roushan <kumar7roushanjha2005@gmail.com>
2023-07-30 20:36:03 +0530

    added more to it

---
+++ CSS/Web_Calc/index.js
@@ -0,0 +1,25 @@
+let screen = document.getElementById("screen");
+let buttons = document.querySelectorAll("button");
+let screenvalue=""
+for(item of buttons){
+    item.addEventListener("click", (e)=>{
+        buttonsText = e.target.innerText;
+        console.log(buttonsText)
+        if(buttonsText=="X"){
+            buttonsText="*";
+            screenvalue+=buttonsText
+            screen.value = screenvalue
+        }
+        else if(buttonsText=="C"){
+            screenvalue= ""
+            screen.value = screenvalue
+        }
+        else if(buttonsText=="="){
+            screen.value = eval(screenvalue)
+        }
+        else{
+            screenvalue+=buttonsText
+            screen.value = screenvalue
+        }
+    })
+}
